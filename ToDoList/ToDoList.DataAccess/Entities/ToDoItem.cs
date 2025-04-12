39e9ee1143c8217a069fed65e02d299ec958a428
Nomozov <kamoliddinnomozov2606@gmail.com>
2025-04-12 12:08:37 +0500

    Entity added

--- ToDoList/ToDoList.DataAccess/Entities/ToDoItem.cs
+++ ToDoList/ToDoList.DataAccess/Entities/ToDoItem.cs
@@ -12,7 +12,7 @@ public class ToDoItem
     public string Title { get; set; }
     public string Description { get; set; }
     public bool IsCompleted { get; set; }
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-    public DateTime? CompletedAt { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime DueDate { get; set; }
    
 }
