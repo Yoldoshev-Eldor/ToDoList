939b49c03027a8955057cc558b0541cf9dbbe7c0
Saidabror Siddiqjonov <siddiqjonovsaidabror@gmail.com>
2025-04-13 16:46:41 +0500

    repo, service and api implemented for adding item

---
+++ ToDoList/ToDoList.Service/FluentValidation/ToDoItemCreateDtoValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using ToDoList.Service.DTOs;
+
+namespace ToDoList.Service.FluentValidation;
+
+public class ToDoItemCreateDtoValidator : AbstractValidator<ToDoItemCreateDto>
+{
+    public ToDoItemCreateDtoValidator()
+    {
+        RuleFor(t => t.Title).
+            MaximumLength(50).
+            NotEmpty().
+            WithMessage("Title is required");
+
+        RuleFor(t => t.Description).
+            MaximumLength(255).
+            NotEmpty().
+            WithMessage("Description is required");
+
+        RuleFor(t => t.DueDate).
+            NotEmpty().
+            NotNull().
+            WithMessage("DueDate is required");
+    }
+}
--- ToDoList/ToDoList.Service/Services/ToDoItemService.cs
+++ ToDoList/ToDoList.Service/Services/ToDoItemService.cs
@@ -1,12 +1,42 @@
-﻿using ToDoList.Service.DTOs;
+﻿using ToDoList.DataAccess.Entities;
+using ToDoList.Repository.Services;
+using ToDoList.Service.DTOs;
+using ToDoList.Service.FluentValidation;
 
 namespace ToDoList.Service.Services;
 
 public class ToDoItemService : IToDoItemService
 {
-    public Task AddToDoItemAsync(ToDoItemCreateDto toDoItem)
+    private readonly IToDoItemRepository ToDoItemRepository;
+
+    public ToDoItemService(IToDoItemRepository toDoItemRepository)
     {
-        throw new NotImplementedException();
+        ToDoItemRepository = toDoItemRepository;
+    }
+
+    public async Task<long> AddToDoItemAsync(ToDoItemCreateDto toDoItemDto)
+    {
+        var validator = new ToDoItemCreateDtoValidator();
+        var result = validator.Validate(toDoItemDto);
+
+        if (!result.IsValid)
+            throw new Exception(string.Join("; ", result.Errors.Select(e => e.ErrorMessage)));
+
+        var toDoItem = ConvertToDoItemCreateDtoToToDoItem(toDoItemDto);
+        var toDoItemId = await ToDoItemRepository.AddToDoItemAsync(toDoItem);
+        return toDoItemId;
+    }
+
+    private ToDoItem ConvertToDoItemCreateDtoToToDoItem(ToDoItemCreateDto toDoItemDto)
+    {
+        return new ToDoItem()
+        {
+            Title = toDoItemDto.Title,
+            Description = toDoItemDto.Description,
+            IsCompleted = false,
+            CreatedAt = DateTime.UtcNow,
+            DueDate = toDoItemDto.DueDate,
+        };
     }
 
     public Task DeleteToDoItemByIdAsync(long Id)
