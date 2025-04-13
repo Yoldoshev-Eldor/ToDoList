596a407c1e493ee4c5b7ca902a2cac8d78522621
AzamatVaxobov <azamatvaxobov104@gmail.com>
2025-04-13 12:01:55 +0500

    code first approach done

--- ToDoList/ToDoList.DataAccess/MainContext.cs
+++ ToDoList/ToDoList.DataAccess/MainContext.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
+using ToDoList.DataAccess.Entities;
 
 namespace ToDoList.DataAccess
 {
     public class MainContext : DbContext
     {
+        public DbSet<ToDoItem> ToDoItems { get; set; }
+        
+
+        public MainContext(DbContextOptions<MainContext> options) : base(options)
+        {
+        }
     }
 }
