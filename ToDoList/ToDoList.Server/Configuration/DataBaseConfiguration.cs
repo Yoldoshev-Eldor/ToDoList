9ad76bf572129731177650e9fdacf09446ef920a
Yoldoshev-Eldor <yoldosheveldor05@gmai.com>
2025-04-13 14:13:55 +0500

    Add dearContaner registr dervice and connection string add

---
+++ ToDoList/ToDoList.Server/Configuration/DataBaseConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoList.DataAccess;
+
+namespace ToDoList.Server.Configuration;
+
+public static class DataBaseConfiguration
+{
+    public static void ConfigureDatabase(this WebApplicationBuilder builder)
+    {
+        var connectionConfiguration = builder.Configuration.GetConnectionString("DatabaseConnection");
+
+        builder.Services.AddDbContext<MainContext>(options => options.UseSqlServer(connectionConfiguration));
+    }
+}
