3fd18b46d58376b53a8242f7bfbf2c812d83c682
Saidabror Siddiqjonov <siddiqjonovsaidabror@gmail.com>
2025-05-04 20:03:04 +0500

    FluentApi configurations

---
+++ ToDoList/ToDoList.DataAccess/EntityConfiguration/ToDoItemConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ToDoList.DataAccess.Entities;
+
+namespace ToDoList.DataAccess.EntityConfiguration;
+
+public class ToDoItemConfiguration : IEntityTypeConfiguration<ToDoItem>
+{
+    public void Configure(EntityTypeBuilder<ToDoItem> builder)
+    {
+        builder.ToTable("ToDoItem");
+        builder.HasKey(i => i.Id);
+
+        builder.Property(i => i.Title).HasMaxLength(50).IsRequired(true);
+        builder.Property(i => i.Description).HasMaxLength(255).IsRequired(false);
+        builder.Property(i => i.IsCompleted).IsRequired(true);
+        builder.Property(i => i.CreatedAt).IsRequired(true);
+        builder.Property(i => i.DueDate).IsRequired(true);
+    }
+}
