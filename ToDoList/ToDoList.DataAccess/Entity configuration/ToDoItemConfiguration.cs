4c9c67346ffdcf056a21eb0968805ca34063f993
Sherlock <shholmes359@gmail.com>
2025-05-05 04:07:31 +0500

    entity configuration added

---
+++ ToDoList/ToDoList.DataAccess/Entity configuration/ToDoItemConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ToDoList.DataAccess.Entities;
+
+namespace ToDoList.DataAccess.Entity_configuration
+{
+    public class ToDoItemConfiguration : IEntityTypeConfiguration<ToDoItem>
+    {
+        public void Configure(EntityTypeBuilder<ToDoItem> builder)
+        {
+            builder.ToTable("ToDoItem");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Title)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            builder.Property(x => x.Description)
+                .HasMaxLength(1000);
+
+            builder.Property(x => x.IsCompleted)
+                .IsRequired();
+
+            builder.Property(x => x.CreatedAt)
+                .IsRequired();
+
+            builder.Property(x => x.DueDate)
+                .IsRequired();
+        }
+    }
+}
