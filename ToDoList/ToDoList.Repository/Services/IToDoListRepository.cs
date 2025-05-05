d1a15262932e71b6206f55971c3b0c4cde298d74
Sherlock <shholmes359@gmail.com>
2025-05-06 01:44:45 +0500

    repository service added

--- ToDoList/ToDoList.Repository/Services/IToDoListRepository.cs
+++ ToDoList/ToDoList.Repository/Services/IToDoListRepository.cs
@@ -1,5 +1,15 @@
-﻿namespace ToDoList.Repository.Services;
+﻿using ToDoList.DataAccess.Entities;
+
+namespace ToDoList.Repository.Services;
 
 public interface IToDoListRepository
 {
+    Task<long> AddToDoItemAsync(ToDoItem toDoItem);
+    Task DeleteToDoItemByIdAsync(long Id);
+    Task UpdateToDoItemAsync(ToDoItem toDoItem);
+    Task<List<ToDoItem>> SelectAllToDoItemsAsync(int skip, int take);
+    Task<ToDoItem> SelectToDoItemByIdAsync(long Id);
+    Task<List<ToDoItem>> SelectByDueDateAsync(DateTime dateTime);
+    Task<List<ToDoItem>> SelectCompletedAsync(int skip, int take);
+    Task<List<ToDoItem>> SelectIncompletedAsync(int skip, int take);
 }
