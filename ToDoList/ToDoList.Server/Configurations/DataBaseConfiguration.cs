d7b32da36cb963673cb0f78b0eef613df9a3194f
Yoldoshev-Eldor <yoldosheveldor05@gmai.com>
2025-05-05 14:35:35 +0500

    Add migartion

--- ToDoList/ToDoList.Server/Configurations/DataBaseConfiguration.cs
+++ ToDoList/ToDoList.Server/Configurations/DataBaseConfiguration.cs
@@ -1,12 +1,15 @@
-﻿namespace ToDoList.Server.Configurations;
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoList.DataAccess;
+
+namespace ToDoList.Server.Configurations;
 
 
 public static class DataBaseConfiguration
 {
-    //public static void ConfigureDatabase(this WebApplicationBuilder builder)
-    //{
-    //    var connectionConfiguration = builder.Configuration.GetConnectionString("DatabaseConnection");
+    public static void ConfigureDatabase(this WebApplicationBuilder builder)
+    {
+        var connectionConfiguration = builder.Configuration.GetConnectionString("DatabaseConnection");
 
-    //    builder.Services.AddDbContext<MainContext>(options => options.UseSqlServer(connectionConfiguration));
-    //}
+        builder.Services.AddDbContext<MainContext>(options => options.UseSqlServer(connectionConfiguration));
+    }
 }
--- ToDoList/ToDoList.Server/Program.cs
+++ ToDoList/ToDoList.Server/Program.cs
@@ -1,4 +1,6 @@
 
+using ToDoList.Server.Configurations;
+
 namespace ToDoList.Server
 {
     public class Program
@@ -13,7 +15,7 @@ namespace ToDoList.Server
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
+            builder.ConfigureDatabase();
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
