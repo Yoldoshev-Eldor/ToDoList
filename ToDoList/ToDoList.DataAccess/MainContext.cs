3231d8665ff590a5e5ea0b0c9fefd1c758c2481e
Yoldoshev-Eldor <yoldosheveldor05@gmai.com>
2025-05-05 13:51:57 +0500

    add configur

---
+++ ToDoList/ToDoList.DataAccess/MainContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoList.DataAccess.Entities;
+using ToDoList.DataAccess.EntityConfigurations;
+
+namespace ToDoList.DataAccess;
+
+public class MainContext : DbContext
+{
+    public DbSet<ToDoItem> ToDoItems { get; set; }
+
+    public MainContext(DbContextOptions<MainContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfiguration(new ToDoItemConfiguration());
+    }
+}
